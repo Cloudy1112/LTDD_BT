610f40901f7637347ecefe3074cd3d16cb9ce0b4
Cloudy1112 <chonglevan2004@gmai.com>
2025-11-07 14:48:24 +0700

    Cau 1 va 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation("de.hdodenhof.circleimageview:circleimageview:3.1.0") //thư viện để tạo ảnh góc bo tròn
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("de.hdodenhof:circleimageview:3.1.0") //thư viện để tạo ảnh góc bo tròn
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
